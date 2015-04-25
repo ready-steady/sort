9c3cfe7a1d7c02f874e1fea94980e5122ad0cd02
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-25 20:09:03 +0200

    Implement Unique for sorting and eliminating duplicates

--- main.go
+++ main.go
@@ -1,2 +1,22 @@
 // Package sort provides sorting algorithms.
 package sort
+
+import (
+	"sort"
+)
+
+// Unique sorts data in place, eliminates duplicates, and returns the number of
+// unique elements.
+func Unique(data []float64) uint {
+	sort.Float64s(data)
+
+	n, k := uint(len(data)), uint(0)
+	for i := uint(1); i < n; i++ {
+		if data[k] != data[i] {
+			k++
+			data[k] = data[i]
+		}
+	}
+
+	return k + 1
+}
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package sort
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/ready-steady/assert"
+)
+
+func TestUnique(t *testing.T) {
+	data := []float64{1, -1, 2, 0, 0, 2, 4, 1, 1, 1, 4}
+	assert.Equal(data[:Unique(data)], []float64{-1, 0, 1, 2, 4}, t)
+}
+
+func BenchmarkUnique(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Unique(random(1e6, 10))
+	}
+}
+
+func random(count, unique uint) []float64 {
+	data := make([]float64, count)
+	for i := range data {
+		data[i] = float64(uint(float64(unique) * rand.Float64()))
+	}
+	return data
+}
